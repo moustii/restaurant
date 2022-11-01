2a30b7e2418f723b725e2c478b39c7569e02fdcf
Jeremy Ibert <jeremy.ibert@outlook.com>
2022-11-01 21:52:26 +0100

    add new files and images

---
+++ Views/common/template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FullSnack'</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://bootswatch.com/5/cyborg/bootstrap.min.css">
+</head>
+<body>
+    <header>
+        <?php require_once 'Views/common/menu.php' ?>
+    </header>
+
+
+    <div class="container">
+        
+    </div>
+
+
+
+
+
+
+    <footer>
+
+    </footer>
+</body>
+</html>
